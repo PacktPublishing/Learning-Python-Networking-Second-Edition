8ef547eb6bf0dbe44c22fbbdcd16213e52ac9e14
Ortega Candel <jose-manuel.ortega-candel@capgemini.com>
2019-03-06 13:37:37 +0100

    Learning-Python-Networking-Second-Edition

--- chapter3/twitter_api/twitter_mentions.py
+++ chapter3/twitter_api/twitter_mentions.py
@@ -32,6 +32,8 @@ def get_mentions(since_id, auth_obj):
 	params = {'count': 200, 'since_id': since_id,'include_rts': 0, 'include_entities': 'false'}
 	url = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json'
 	response = requests.get(url, params=params, auth=auth_obj)
+	#Checking if the request is successful.
+    #It will raise an HTTPError if the request returned an unsuccessful status code.
 	response.raise_for_status()
 	return json.loads(response.text)
 
