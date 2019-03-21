7be670979eb3e939c797089979b036ea3c4a3408
Ortega Candel <jose-manuel.ortega-candel@capgemini.com>
2019-03-21 15:40:07 +0100

    Learning-Python-Networking-Second-Edition

--- chapter11/asyncio_aiohttp/asyncio_task.py
+++ chapter11/asyncio_aiohttp/asyncio_task.py
@@ -5,17 +5,17 @@ import time
 
 @asyncio.coroutine
 def task_sleep(name, loop, seconds=1):
-        future = loop.run_in_executor(None, time.sleep, seconds)
-        print("[%s] coroutine will sleep for %d second(s)..." % (name, seconds))
-        yield from future
-        print("[%s] done!" % name)
+	future = loop.run_in_executor(None, time.sleep, seconds)
+	print("[%s] coroutine will sleep for %d second(s)..." % (name, seconds))
+	yield from future
+	print("[%s] done!" % name)
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+	loop = asyncio.get_event_loop()
 
-    tasks = [asyncio.Task(task_sleep('Task-A', loop, 10)),
-                asyncio.Task(task_sleep('Task-B', loop,5)),
+	tasks = [asyncio.Task(task_sleep('Task-A', loop, 10)),
+             asyncio.Task(task_sleep('Task-B', loop,5)),
                 asyncio.Task(task_sleep('Task-C', loop))]
-      
-    loop.run_until_complete(asyncio.gather(*tasks))
+
+	loop.run_until_complete(asyncio.gather(*tasks))
--- chapter11/tornado/tornado_request_async.py
+++ chapter11/tornado/tornado_request_async.py
@@ -5,16 +5,16 @@ import tornado.web
 import tornado.httpclient
 
 class Handler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    def get(self):
-       http_client = tornado.httpclient.AsyncHTTPClient()
-       http_client.fetch("https://www.google.com/search?q=python", callback=self.on_response)
+	@tornado.web.asynchronous
+	def get(self):
+		http_client = tornado.httpclient.AsyncHTTPClient()
+		http_client.fetch("https://www.google.com/search?q=python", callback=self.on_response)
 
-    def on_response(self, response):
-       self.write(response.body)
-       self.finish()
+	def on_response(self, response):
+		self.write(response.body)
+		self.finish()
 
 if __name__ == '__main__':	   
 	app = tornado.web.Application([ tornado.web.url(r"/", Handler)])
-	app.listen(8080)
+	app.listen(8081)
 	tornado.ioloop.IOLoop.current().start()
